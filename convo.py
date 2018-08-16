7c3ffc6770fa1516c1efaa2b3b6091f67d185049
Charles Tousignant <ctousignant84@gmail.com>
2018-08-16 13:50:15 -0400

    16 aout

--- convo.py
+++ convo.py
@@ -1,13 +1,13 @@
-from prepare_data import *
-import matplotlib.pyplot as plt
-import tensorflow as tf
 import glob
+from utils import *
+
 #np.set_printoptions(threshold=np.nan)
 
 trainDSG = DataSetGenerator("./train")
 testDSG = DataSetGenerator("./test")
-photosDSG = DataSetGenerator("./2016test")
 
+learning_rate = 0.001
+epochs = 200
 
 height = 48
 width = 48
@@ -25,10 +25,10 @@ for data in test_data:
     Xtest = data[0]/255
     Ytest = data[1]
 
-
+print("Importing data to predict...")
 # Import data to predict
 image_list, image_name = [], []
-photo_path = "./2016test/*.jpg"
+photo_path = "./photos/*.jpg"
 for filename in glob.glob(photo_path):
     if os.path.getsize(filename) > 0:
         image_name.append(filename[len(photo_path)-5:])
@@ -37,49 +37,14 @@ for filename in glob.glob(photo_path):
         im = im.reshape(1, height, width, 3)/255
         image_list.append(im)
 Xphotos = np.vstack(image_list)
-print(len(Xphotos))
-Yphotos = np.zeros((len(Xphotos), classes))
-print(Xphotos.shape)
-print(Yphotos.shape)
+#Yphotos = np.zeros((len(Xphotos), classes))
 
 
+print("Creating model...")
 # Creating model
 x = tf.placeholder(tf.float32, shape=[None, height, width, 3])
 y_true = tf.placeholder(tf.float32, shape=[None, classes])
 hold_prob = tf.placeholder(tf.float32)
-
-# Helper Functions
-
-
-def init_weights(shape):
-    init_random_dist = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(init_random_dist)
-
-
-def init_bias(shape):
-    init_bias_vals = tf.constant(0.1, shape=shape)
-    return tf.Variable(init_bias_vals)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2by2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-
-def convolutional_layer(input_x, shape):
-    W = init_weights(shape)
-    b = init_bias([shape[3]])
-    return tf.nn.relu(conv2d(input_x, W) + b)
-
-
-def normal_full_layer(input_layer, size):
-    input_size = int(input_layer.get_shape()[1])
-    W = init_weights([input_size, size])
-    b = init_bias([size])
-    return tf.matmul(input_layer, W) + b
 
 
 # Create layers
@@ -102,50 +67,41 @@ y_pred = normal_full_layer(full_one_dropout, classes)
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred))
 
 # Optimizer
-optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train = optimizer.minimize(cross_entropy)
 
-#init = tf.global_variables_initializer()
-
+print("Graph session...")
 # Graph session
-
+init = tf.global_variables_initializer()
 saver = tf.train.Saver()
 
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-
-    epochs = 200
-    for i in range(epochs):
-        #batch = ch.next_batch(100)
-        sess.run(train, feed_dict={x: X, y_true: Y, hold_prob: 0.5})
-
-        # PRINT OUT A MESSAGE EVERY 10 STEPS
-        if i % 10 == 0:
-            print('Currently on step {}'.format(i))
-            print('Accuracy is:')
-            # Test the Train Model
-            matches = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_true, 1))
-
-            acc = tf.reduce_mean(tf.cast(matches, tf.float32))
-
-            print(sess.run(acc, feed_dict={x: Xtest, y_true: Ytest, hold_prob: 1.0}))
-            print('\n')
-
-
-
-    saver.save(sess, 'models/model.ckpt')
-    # saver.restore(sess, 'models/model48.ckpt')
+    sess.run(init)
+    # for i in range(epochs):
+    #     sess.run(train, feed_dict={x: X, y_true: Y, hold_prob: 0.5})
+    #     if i % 10 == 0:
+    #         print('Currently on step {}'.format(i))
+    #         print('Accuracy is:')
+    #         # Test the Train Model
+    #         matches = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_true, 1))
+    #
+    #         acc = tf.reduce_mean(tf.cast(matches, tf.float32))
+    #
+    #         print(sess.run(acc, feed_dict={x: Xtest, y_true: Ytest, hold_prob: 1.0}))
+    #         print('\n')
+    #
+    # saver.save(sess, 'models/model.ckpt')
+    saver.restore(sess, 'models/model.ckpt')
 
     # Use the model to get predicted y
-    #result = sess.run(y_pred, feed_dict={x: Xphotos, y_true: Yphotos, hold_prob: 1.0})
     result = sess.run(y_pred, feed_dict={x: Xphotos, hold_prob: 1.0})
     result_list = list(result)
 
 
+print("Creating .txt file with results...")
 text_file = open("output/Output.txt", "w")
 for i in range(len(image_name)):
-    text_file.write("{} {},{}\n".format(i+1, image_name[i], result_list[i].argmax()))
-
+    text_file.write("{},{}\n".format(image_name[i], result_list[i].argmax()))
 text_file.close()
 
 
--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
-import cv2
-import numpy as np
-from os.path import isfile, join
-from os import listdir
-from random import shuffle
-from shutil import copyfile
 import os
 import pickle
+from os import listdir
+from os.path import isfile, join
+from random import shuffle
+from shutil import copyfile
+import cv2
+import numpy as np
+import tensorflow as tf
 
 
 def separateData(data_dir):
@@ -101,7 +102,7 @@ class DataSetGenerator:
                     continue
                 empty = False
                 img = cv2.imread(self.data_info[i][counter])
-                img = self.resizeAndPad(img, image_size)
+                img = resizeAndPad(img, image_size)
                 if not allchannel:
                     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                     img = np.reshape(img, (img.shape[0], img.shape[1], 1))
@@ -112,50 +113,44 @@ class DataSetGenerator:
             if empty:
                 break
             # if the iterator is multiple of batch size return the mini batch
-            if (counter) % each_batch_size == 0:
+            if counter % each_batch_size == 0:
                 yield np.array(images, dtype=np.uint8), np.array(labels, dtype=np.uint8)
                 del images
                 del labels
                 images = []
                 labels = []
 
-    def resizeAndPad(self, img, size):
-        h, w = img.shape[:2]
 
-        sh, sw = size
-        # interpolation method
-        if h > sh or w > sw:  # shrinking image
-            interp = cv2.INTER_AREA
-        else:  # stretching image
-            interp = cv2.INTER_CUBIC
+# Helper Functions
+def init_weights(shape):
+    init_random_dist = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(init_random_dist)
 
-        # aspect ratio of image
-        aspect = w / h
 
-        # padding
-        if aspect > 1:  # horizontal image
-            new_shape = list(img.shape)
-            new_shape[0] = w
-            new_shape[1] = w
-            new_shape = tuple(new_shape)
-            new_img = np.zeros(new_shape, dtype=np.uint8)
-            h_offset = int((w - h) / 2)
-            new_img[h_offset:h_offset + h, :, :] = img.copy()
+def init_bias(shape):
+    init_bias_vals = tf.constant(0.1, shape=shape)
+    return tf.Variable(init_bias_vals)
 
-        elif aspect < 1:  # vertical image
-            new_shape = list(img.shape)
-            new_shape[0] = h
-            new_shape[1] = h
-            new_shape = tuple(new_shape)
-            new_img = np.zeros(new_shape, dtype=np.uint8)
-            w_offset = int((h - w) / 2)
-            new_img[:, w_offset:w_offset + w, :] = img.copy()
-        else:
-            new_img = img.copy()
-        # scale and pad
-        scaled_img = cv2.resize(new_img, size, interpolation=interp)
-        return scaled_img
 
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2by2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+def convolutional_layer(input_x, shape):
+    W = init_weights(shape)
+    b = init_bias([shape[3]])
+    return tf.nn.relu(conv2d(input_x, W) + b)
+
+
+def normal_full_layer(input_layer, size):
+    input_size = int(input_layer.get_shape()[1])
+    W = init_weights([input_size, size])
+    b = init_bias([size])
+    return tf.matmul(input_layer, W) + b
 
 # if __name__ == "__main__":
 #     separateData("./train")
